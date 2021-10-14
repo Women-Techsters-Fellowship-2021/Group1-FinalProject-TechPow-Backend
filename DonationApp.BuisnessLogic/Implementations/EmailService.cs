2a86469b7bde9b6b284087bf100c1ea3bbb672ae
Dhanidayo <sarahjospeh@ymail.com>
2021-10-14 10:20:20 +0100

    fixed bugs

--- DonationApp.BuisnessLogic/Implementations/EmailService.cs
+++ DonationApp.BuisnessLogic/Implementations/EmailService.cs
@@ -5,9 +5,11 @@ using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.Extensions.Options;
 using MimeKit;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,9 +20,9 @@ namespace DonationApp.BuisnessLogic.Implementations
         private readonly MailSettings _mailSettings;
         public EmailService(IOptions<MailSettings> options)
         {
-            _mailSettings =options.Value;  
+            _mailSettings = options.Value;
         }
-        
+
         public async Task SendEmail(EmailRequestDTO emailRequestDTO)
         {
             var email = new MimeMessage();
@@ -31,6 +33,22 @@ namespace DonationApp.BuisnessLogic.Implementations
 
             bodybuilder.HtmlBody = emailRequestDTO.Body;
             email.Body = bodybuilder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+        }
+
+        public async Task SendOTPEmail(string userEmail, int otp)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(userEmail));
+            email.Subject = "Password Reset OTP";
+            var bodybuilder = new BodyBuilder();
+            bodybuilder.HtmlBody = "Dear " + userEmail + " <br><br> Please find the reset password token below<br><br> " + otp + " <br><br><br> Thanks <br> TechPow Team";
+            email.Body = bodybuilder.ToMessageBody();
+           
             using var smtp = new SmtpClient();
             smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
             smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
