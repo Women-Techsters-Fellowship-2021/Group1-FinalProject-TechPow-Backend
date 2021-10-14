201b3b43eb85b07ec6d4b5af37523d0e59907aca
Dhanidayo <sarahjospeh@ymail.com>
2021-10-14 10:14:34 +0100

    fixed bugs

--- DonationApp.BuisnessLogic/Interfaces/IPasswordService.cs
+++ DonationApp.BuisnessLogic/Interfaces/IPasswordService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DonationApp.Models;
+using DonationApp.DTO.AppuserDTOs;
 
 namespace DonationApp.BuisnessLogic.Interfaces
 {
